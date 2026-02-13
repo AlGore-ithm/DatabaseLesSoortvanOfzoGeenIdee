a9585fc3b4762495a30fe0696474eeb88434da66
Yanick <73784319+AlGore-ithm@users.noreply.github.com>
2026-02-13 12:18:02 +0100

    wowowowow

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using MySqlConnector;
+using System;
+
+Console.WriteLine("Hello, World!");
+string Databasename = "company";
+string Username = "schoolofzo";
+string Password = "kali123";
+string Server = "192.168.30.128";
+string Port = "3306";
+string ConnectionString = $"Server={Server};Port={Port};Database={Databasename};Uid={Username};Pwd={Password};";
+MySqlConnection conn = new MySqlConnection(ConnectionString);
+conn.Open();
+string sql = "SELECT name, description FROM department;";
+using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+{
+    MySqlDataReader reader = cmd.ExecuteReader();
+    while (reader.Read())
+    {
+        string name = reader.GetString("name");
+        string description = reader.GetString("description");
+        Console.WriteLine("{0} = {1}", name, description);
+    }
+    reader.Close();
+}
+
+conn.Close();
